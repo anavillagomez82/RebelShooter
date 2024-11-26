c3cd7ab6bae669eeac653fb225e5d67b4e69aefd
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-26 18:41:59 -0500

    Update Enemie.h

--- Enemie.h
+++ Enemie.h
@@ -2,25 +2,46 @@
 #include <SFML/Graphics.hpp>
 #ifndef ENEMIE_H
 #define ENEMIE_H
-
+#include "Entity.h"
 using namespace std;
 using namespace sf;
 
-class Enemie : public Drawable{
-    private:
-        Sprite sprite;
-        bool activado;
-        float vel;
-        int Hp;
-        int dist;
-        void Movement(Vector2f);
-        void apuntarPlayer(Vector2f);
-    public:
-        Enemie(Vector2f,Texture&);
-        void Update(Vector2f,int);
-        Vector2f GetPosition();
-        void TakeHp();
-        int ConsultHp();
-        virtual void draw(RenderTarget&,RenderStates) const;
+class Enemy : public Entity {
+private:
+    bool isAggressive;
+
+public:
+    // Constructor del enemigo
+    Enemy(sf::Texture& texture, sf::Vector2f position)
+        : Entity(texture, position, 2.f), isAggressive(false) {}
+
+    // Implementación de la función Update específica para el enemigo
+    void Update() override {
+        if (isAggressive) {
+            chasePlayer();
+        } else {
+            randomMovement();
+        }
+
+        Move();
+    }
+
+    // Método para hacer que el enemigo persiga al jugador
+    void chasePlayer() {
+        sf::Vector2f playerPosition(500.f, 300.f);  // Ejemplo de posición del jugador (debes usar la posición real)
+        direction = playerPosition - sprite.getPosition();
+        direction /= std::sqrt(direction.x * direction.x + direction.y * direction.y);  // Normalizar la dirección
+    }
+
+    // Movimiento aleatorio del enemigo
+    void randomMovement() {
+        direction = sf::Vector2f(rand() % 3 - 1, rand() % 3 - 1);  // Movimiento aleatorio [-1, 1]
+    }
+
+    // Cambiar el estado de agresividad del enemigo
+    void setAggression(bool aggressive) {
+        isAggressive = aggressive;
+    }
 };
-#endif 
+
+#endif
