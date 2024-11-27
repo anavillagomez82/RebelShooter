962e02fea50b5dbf026a84f426061d34d0d61576
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-26 23:39:23 -0500

    Update GameEntity.h

--- GameEntity.h
+++ GameEntity.h
@@ -8,6 +8,7 @@ public:
     virtual ~GameEntity() {} // Destructor virtual
     virtual Vector2f GetPosition() const = 0; // Método puro virtual para obtener la posición
     virtual void Update() = 0; // Método puro virtual para actualizar la entidad
+    virtual void draw(RenderTarget& target, RenderStates states) const override = 0; // Método virtual puro
 };
 
 #endif
