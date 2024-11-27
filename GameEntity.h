deaad4eaeec3f66b56476fc7ee1a7a21b7bd7434
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-27 00:11:56 -0500

    Update GameEntity.h

--- GameEntity.h
+++ GameEntity.h
@@ -6,7 +6,7 @@ using namespace sf;
 
 class GameEntity : public Drawable {
 public:
-    virtual ~GameEntity() {} // Destructor virtual
+    virtual ~GameEntity() = default; // Destructor virtual
     virtual Vector2f GetPosition() const = 0; // Método puro virtual para obtener la posición
     virtual void Update() = 0; // Método puro virtual para actualizar la entidad
     virtual void draw(RenderTarget& target, RenderStates states) const override = 0; // Método virtual puro
