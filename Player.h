0dd28859cac5fedc5f3e9e3e96bf4ab55e546572
anavillagomez82 <ana.villagomez@ucsp.edu.pe>
2024-11-26 23:12:14 -0500

    Update Player.h

--- Player.h
+++ Player.h
@@ -2,57 +2,22 @@
 #include <SFML/Graphics.hpp>
 #ifndef PLAYER_H
 #define PLAYER_H
-#include "Entity.h"
-using namespace std;
-using namespace sf;
+#include "GameEntity.h"
 
-
-class Player : public Entity {
-private:
-    bool isShooting;
-
+class Player : public GameEntity {
 public:
-    // Constructor del jugador
-    Player(sf::Texture& texture, sf::Vector2f position)
-        : Entity(texture, position, 5.f), isShooting(false) {}
+    Player(Texture&);
+    void Update(RenderWindow&, View&) override; // Sobrescribir el método Update
+    Vector2f GetPosition() const override; // Sobrescribir el método GetPosition
+    pair<bool, Vector2f> AnswerShoot(RenderWindow&); // Método específico de Player
+    float GetRotation(); // Método específico de Player
+    virtual void draw(RenderTarget&, RenderStates) const override;
 
-    // Implementación de la función Update específica para el jugador
-    void Update() override {
-        // Lógica de movimiento del jugador
-        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
-            direction = sf::Vector2f(0.f, -1.f);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
-            direction = sf::Vector2f(0.f, 1.f);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-            direction = sf::Vector2f(-1.f, 0.f);
-        } else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-            direction = sf::Vector2f(1.f, 0.f);
-        } else {
-            direction = sf::Vector2f(0.f, 0.f);
-        }
-
-        Move();
-
-        // Lógica de disparo
-        if (isShooting) {
-            shoot();
-        }
-    }
-
-    // Función para simular disparos
-    void shoot() {
-        std::cout << "El jugador está disparando!" << std::endl;
-    }
-
-    // Método para activar o desactivar el disparo
-    void setShooting(bool shooting) {
-        isShooting = shooting;
-    }
-
-    // Obtener la posición del jugador
-    sf::Vector2f GetPlayerPosition() const {
-        return sprite.getPosition();
-    }
+private:
+    Sprite sprite;
+    float vel;
+    int cadencia = 8;
+    void Movement(View&);
+    void apuntar(RenderWindow&);
 };
-
 #endif
